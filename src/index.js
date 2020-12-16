e5cb1827a9a078b4e7d1499f7471c86cf4e63d8b
Vinay846 <Vinayvin84629@gmail.com>
2020-12-16 22:44:10 +0530

    Solution

--- src/index.js
+++ src/index.js
@@ -11,23 +11,23 @@ const { data } = require('./data')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+const sanitize = (value, defaultValue)=>{
+    if(value === null || value === undefined || isNaN(Number(value))){
+        return defaultValue;
+    }
+    return value;
+}
+
 // your code goes here
 app.get("/topRankings", (req, res)=>{
-    let offset = parseInt(req.query.offset);
-    let limit = parseInt(req.query.limit);
+    const offset = sanitize(req.query.offset, 0);
+    const limit = sanitize(req.query.limit, 20);
     
-    if(isNaN(offset) || offset < 0){
-        offset = 0;
-    }if(isNaN(limit) || limit < 0){
-        limit = 20;
-    }
-    let toSend = [];
-    for(let i=offset; i<limit+offset; i++){
-        // toSend.push(i);
-        toSend.push(data[i]);
-    }
+    res.send(data.filter((ele, idx)=>{
+        return idx >= offset && idx < offset + limit;
+    }));
 
-    res.send(toSend);
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
